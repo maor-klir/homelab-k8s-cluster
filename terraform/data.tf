ca62c829784382ad8c2250e6a773637e81793b54
Maor Klir <maor.klir@gmail.com>
2025-11-30 12:38:29 +0100

    feat: import Terraform code from a private repository
    Signed-off-by: Maor Klir <maor.klir@gmail.com>

---
+++ terraform/data.tf
@@ -0,0 +1,5 @@
+# data "proxmox_virtual_environment_vm" "k3s-ubuntu-template" {
+#   node_name = var.pve_node_name.node_01
+#   vm_id     = 9001
+#   template  = true
+# }
--- terraform/pve-env-file-snippets/outputs.tf
+++
@@ -1,4 +0,0 @@
-
-output "pve-env-file-cp-id" {
-  value = proxmox_virtual_environment_file.pve-env-file-cp.id
-}
