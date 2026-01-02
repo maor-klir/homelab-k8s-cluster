68ce865a76b63b998466160558ceb70e33b8ce07
Maor Klir <maor.klir@gmail.com>
2026-01-02 19:47:15 +0100

    feat: add Terraform outputs to the K3s QA cluster
    Signed-off-by: Maor Klir <maor.klir@gmail.com>

---
+++ terraform/environments/qa/outputs.tf
@@ -0,0 +1,3 @@
+output "k3s_nodes" {
+  value = module.k3s_cluster.k3s_nodes
+}
--- terraform/modules/k3s-cluster/outputs.tf
+++ terraform/modules/k3s-cluster/outputs.tf
@@ -1,25 +1,11 @@
-output "control_plane_nodes" {
-  description = "Map of control plane node details"
+output "k3s_nodes" {
+  description = "Map of all node details"
   value = {
     for k, v in proxmox_virtual_environment_vm.k3s_nodes :
     k => {
       name       = v.name
-      vm_id      = v.vm_id
+      role       = contains(v.tags, "control-plane") ? "control-plane" : "worker"
       ip_address = v.initialization[0].ip_config[0].ipv4[0].address
     }
-    if contains(v.tags, "control-plane")
-  }
-}
-
-output "worker_nodes" {
-  description = "Map of worker node details"
-  value = {
-    for k, v in proxmox_virtual_environment_vm.k3s_nodes :
-    k => {
-      name       = v.name
-      vm_id      = v.vm_id
-      ip_address = v.initialization[0].ip_config[0].ipv4[0].address
-    }
-    if contains(v.tags, "worker")
   }
 }
