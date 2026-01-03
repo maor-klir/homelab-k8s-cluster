4684ede500b43ce416f113dc3905125284975467
Maor Klir <maor.klir@gmail.com>
2026-01-03 16:54:52 +0100

    chore: add K3s QA Azure workload identity outputs
    Signed-off-by: Maor Klir <maor.klir@gmail.com>

--- terraform/environments/qa/outputs.tf
+++ terraform/environments/qa/outputs.tf
@@ -1,3 +1,36 @@
+# K3s cluster module outputs
+#
 output "k3s_nodes" {
   value = module.k3s_cluster.k3s_nodes
+}
+# Azure Workload Identity module outputs
+#
+output "oidc_issuer_uri" {
+  description = "The OIDC issuer URI"
+  value       = module.azure_workload_identity.oidc_issuer_uri
+}
+
+output "key_vault_id" {
+  description = "The ID of the Azure Key Vault"
+  value       = module.azure_workload_identity.key_vault_id
+}
+
+output "key_vault_name" {
+  description = "The name of the Azure Key Vault"
+  value       = module.azure_workload_identity.key_vault_name
+}
+
+output "service_principal_client_id" {
+  description = "The client ID of the Azure AD service principal"
+  value       = module.azure_workload_identity.service_principal_client_id
+}
+
+output "service_principal_object_id" {
+  description = "The object ID of the Azure AD service principal"
+  value       = module.azure_workload_identity.service_principal_object_id
+}
+
+output "application_id" {
+  description = "The application ID"
+  value       = module.azure_workload_identity.application_id
 }
