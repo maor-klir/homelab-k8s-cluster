88b502fe8e05050726810af7da7070423b7746b2
Maor Klir <maor.klir@gmail.com>
2026-01-31 18:55:13 +0100

    fix: pass Azure Thanos resource group attributes dynamically
    Signed-off-by: Maor Klir <maor.klir@gmail.com>

--- terraform/environments/prod/observability.tf
+++ terraform/environments/prod/observability.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "thanos_rg" {
 
 resource "azurerm_storage_account" "thanos_storage_account" {
   name                            = var.thanos_storage_account_name
-  resource_group_name             = var.thanos_rg
-  location                        = var.azwi_rg_location
+  resource_group_name             = azurerm_resource_group.thanos_rg.name
+  location                        = azurerm_resource_group.thanos_rg.location
   account_tier                    = "Standard"
   account_kind                    = "StorageV2"
   account_replication_type        = "ZRS"
