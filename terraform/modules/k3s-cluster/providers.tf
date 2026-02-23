546205d1c0438c2be2438d60df42dfbdeb9f5010
Maor Klir <162344800+maor-klir@users.noreply.github.com>
2026-02-24 00:06:18 +0100

    chore(deps): update terraform proxmox to ~> 0.96 (#80)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- terraform/modules/k3s-cluster/providers.tf
+++ terraform/modules/k3s-cluster/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.95"
+      version = "~> 0.96"
     }
     tls = {
       source  = "hashicorp/tls"
