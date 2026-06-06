dd5668ef1e415dc2b4f224e4d8e6907b140921bd
Maor Klir <162344800+maor-klir@users.noreply.github.com>
2026-06-06 18:45:48 +0200

    chore(deps): update terraform proxmox to ~> 0.109 (#143)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- terraform/modules/k3s-cluster/providers.tf
+++ terraform/modules/k3s-cluster/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.107"
+      version = "~> 0.109"
     }
     tls = {
       source  = "hashicorp/tls"
